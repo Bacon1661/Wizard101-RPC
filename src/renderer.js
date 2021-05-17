880fdd20c3b52906858ab2cce7171b7a3576c011
Christopher Gwynn <31748254+Bacon1661@users.noreply.github.com>
2021-05-17 12:11:43 -0400

    configure for react

--- src/renderer.js
+++ src/renderer.js
@@ -26,6 +26,11 @@
  * ```
  */
 
-import './index.css';
+ import React from 'react'
+ import ReactDOM from 'react-dom'
+ import App from './App'
+ import './index.css'
+ 
+ ReactDOM.render(<App />, document.getElementById('app'))
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,9 +1,17 @@
 const rules = require('./webpack.rules');
 
 rules.push({
+  test: /\.jsx?$/,
+  exclude: /node_modules/,
+  use: [{ loader: 'babel-loader' }],
+})
+
+rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  exclude: /node_modules/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
 });
+
 
 module.exports = {
   // Put your normal webpack config below here
